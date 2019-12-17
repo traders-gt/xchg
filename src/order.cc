ba5d7236a04d22a732d563810d115ed09024d001
JonathanQuang <jquang1000@gmail.com>
2019-12-17 16:58:43 -0500

    some comments as I attempt to understand what's going on

--- src/order.cc
+++ src/order.cc
@@ -3,10 +3,17 @@
 
 namespace tagt::xchg::order {
 
+/**
+ * @return whether this range's lower is actually lower than upper bound
+ */
 bool Range::is_valid() const {
   return this->lower <= this->upper;
 }
 
+/**
+ * @param other pointer to another Range object
+ * @return a Range object with the highest of the .lower and smallest of the .upper
+ */
 Range Range::overlap(const Range& other) const {
   return {
     .lower = std::max(this->lower, other.lower),
@@ -14,10 +21,16 @@ Range Range::overlap(const Range& other) const {
   };
 }
 
+/**
+ * @return constant Range with the smallest possible price and largest possible price
+ */
 Range Market::range() const {
   return { .lower = PRICE_MIN, .upper = PRICE_MAX };
 }
 
+/**
+ * @return a constant range with this order's price as the upper bound for a buy or lowerbound for a sell
+ */
 Range Limit::range() const {
   switch (this->direction) {
     case Direction::BUY:
@@ -28,6 +41,9 @@ Range Limit::range() const {
   }
 }
 
+/**
+ * @return a constant range with this order's price as the lower bound for a buy or upper bound for a sell
+ */
 Range StopLoss::range() const {
   switch (this->direction) {
     case Direction::BUY:
@@ -38,6 +54,9 @@ Range StopLoss::range() const {
   }
 }
 
+/**
+ * @return a constant range with buy:[stop_price, limit_price] or sell[limit,stop]
+ */
 Range StopLimit::range() const {
   switch (this->direction) {
     case Direction::BUY:
@@ -48,6 +67,10 @@ Range StopLimit::range() const {
   }
 }
 
+/**
+ * @param other pointer to another order
+ * @return true if tickers match, directions are different, range overlaps valid, (no AON flag or this size smaller than other)
+ */
 bool Order::match(const Order& other) const {
   switch (this->direction) {
     case Direction::BUY:
@@ -64,6 +87,10 @@ bool Order::match(const Order& other) const {
   }
 }
 
+/**
+ * @param other pointer to another order
+ * @return Price of lower of overlap if buy, upper of overlap if sell
+ */
 Price Order::get_match_price(const Order& other) const {
   auto my_range = this->range();
   auto their_range = other.range();
